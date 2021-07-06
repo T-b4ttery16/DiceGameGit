7130f755d406986c55f56b69a9daa772ee194220
TERASAKI Tomoya <T-b4ttery16>
2021-07-06 11:27:37 +0900

    Won/lost message modified

--- DiceGame.c
+++ DiceGame.c
@@ -22,8 +22,8 @@ int main(){
   }
   printf("Total value: %d\n",total);
 
-  if(total > 7)printf("You won!\n");
-  else printf("You lost...\n");
+  if(total > 7)printf("%s won!\n",name);
+  else printf("%s lost...\n",name);
   
   return 0;
 }
